d500272cda77bd8cbc062426686c1354fe564ba0
pwjworks <13610513466@163.com>
2020-02-28 20:39:36 +0800

    header动画与逻辑

--- src/common/header/index.jsx
+++ src/common/header/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {
   HeaderWrapper,
   Logo,
@@ -16,80 +16,100 @@ import {
 } from './style'
 import { CSSTransition } from 'react-transition-group'
 import { connect } from 'react-redux'
-import { actionCreators } from './store'
+import { actionCreators, actionTypes } from './store'
 
-const getListArea=(show)=>{
-  if(show){
-    return (
-      <SearchInfo>
-        <SearchInfoTitle>
-          热门搜索
-          <SearchInfoSwitch>换一批</SearchInfoSwitch>
-        </SearchInfoTitle>
-        <SearchInfoList>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-        </SearchInfoList>
-      </SearchInfo>
-    )
-  }else{
-    return null
+class Header extends Component {
+  getListArea() {
+    const {
+      focused,
+      list,
+      page,
+      handleMouseEnter,
+      handleMouseLeave
+    } = this.props
+    const pageList = []
+    const newList = list.toJS()
+    for (let i = (page - 1) * 10; i < page * 10; i++) {
+      pageList.push(
+        <SearchInfoItem key={newList[i]}>{newList[i]}</SearchInfoItem>
+      )
+    }
+    if (focused) {
+      return (
+        <SearchInfo
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <SearchInfoTitle>
+            热门搜索
+            <SearchInfoSwitch>换一批</SearchInfoSwitch>
+          </SearchInfoTitle>
+          <SearchInfoList>{pageList}</SearchInfoList>
+        </SearchInfo>
+      )
+    } else {
+      return null
+    }
   }
-}
-
-const Header = (props) => {
-  return (
-    <HeaderWrapper>
-      <Logo href='/' />
-      <Nav>
-        <NavItem className='left active'>首页</NavItem>
-        <NavItem className='left'>下载app</NavItem>
-        <NavItem className='right'>登录</NavItem>
-        <NavItem className='right'>
-          <i className='iconfont'>&#xe636;</i>
-        </NavItem>
-        <SearchWrapper>
-          <CSSTransition timeout={200} in={props.focused} classNames='slide'>
-            <NavSearch
-              onFocus={props.handleInputFocus}
-              onBlur={props.handleInputBlur}
-              className={props.focused ? 'focused' : ''}
-            ></NavSearch>
-          </CSSTransition>
-          <i className={props.focused ? 'focused iconfont' : 'iconfont'}>
-            &#xe6dd;
-          </i>
-          {getListArea(props.focused)}
-        </SearchWrapper>
-      </Nav>
-      <Addition>
-        <Button className='writing'>
-          <i className='iconfont'>&#xe670;</i>写文章
-        </Button>
-        <Button className='reg'>注册</Button>
-      </Addition>
-    </HeaderWrapper>
-  )
+  render() {
+    const { focused, handleInputFocus, handleInputBlur } = this.props
+    return (
+      <HeaderWrapper>
+        <Logo href='/' />
+        <Nav>
+          <NavItem className='left active'>首页</NavItem>
+          <NavItem className='left'>下载app</NavItem>
+          <NavItem className='right'>登录</NavItem>
+          <NavItem className='right'>
+            <i className='iconfont'>&#xe636;</i>
+          </NavItem>
+          <SearchWrapper>
+            <CSSTransition timeout={200} in={focused} classNames='slide'>
+              <NavSearch
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
+                className={focused ? 'focused' : ''}
+              ></NavSearch>
+            </CSSTransition>
+            <i className={focused ? 'focused iconfont' : 'iconfont'}>
+              &#xe6dd;
+            </i>
+            {this.getListArea(focused)}
+          </SearchWrapper>
+        </Nav>
+        <Addition>
+          <Button className='writing'>
+            <i className='iconfont'>&#xe670;</i>写文章
+          </Button>
+          <Button className='reg'>注册</Button>
+        </Addition>
+      </HeaderWrapper>
+    )
+  }
 }
 
 const mapStateToProps = (state) => {
   return {
-    focused: state.header.get('focused')
+    focused: state.header.get('focused'),
+    list: state.header.get('list'),
+    page: state.header.get('page')
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
     handleInputFocus() {
+      dispatch(actionCreators.getList())
       dispatch(actionCreators.searchFocus())
     },
     handleInputBlur() {
       dispatch(actionCreators.searchBlur())
+    },
+    handleMouseEnter() {
+      dispatch(actionCreators.MouseEnter())
+    },
+    handleMouseLeave() {
+      dispatch(actionTypes.MouseLeave())
     }
   }
 }
--- src/common/header/store/actionCreators.js
+++ src/common/header/store/actionCreators.js
@@ -1,9 +1,39 @@
 import * as actionTypes from './constants'
+import axios from 'axios'
+import {
+  fromJS
+} from 'immutable'
 
-export const searchFocus = () =>({
+const changeList = (data) => ({
+  type: actionTypes.CHANGE_LIST,
+  data: fromJS(data.data),
+  totalPage: Math.ceil(data.length / 10)
+})
+
+export const searchFocus = () => ({
   type: actionTypes.SEARCH_FOCUS
 })
 
 export const searchBlur = () => ({
   type: actionTypes.SEARCH_BLUR
+})
+
+
+export const getList = () => {
+  return (dispatch) => {
+    axios.get('/api/headerList.json').then((res) => {
+      const data = res.data
+      dispatch(changeList(data))
+    }).catch(() => {
+      console.log('error')
+    })
+  }
+}
+
+export const MouseEnter = () => ({
+  type: actionTypes.MOUSE_ENTER
+})
+
+export const MouseLeave = () => ({
+  type: actionTypes.MOUSE_LEAVE
 })
